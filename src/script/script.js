e7e35a4d303fedf3adad0277507ed8bac744dd7c
Rafael <rafael.cesario@sga.pucminas.br>
2023-06-27 11:22:45 -0300

    adição logins

--- src/script/script.js
+++ src/script/script.js
@@ -135,3 +135,17 @@ document.getElementById("openModal").addEventListener("click", function() {
     document.getElementById("modal").style.display = "none";
   });
 //>>>>>>> 5439d0aa4a25c3aab68b1c7e602a7c18dc0d8250:src/script.js
+
+
+function logar (){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+    }
+    else{
+        alert('Usuário ou senha incorreto')
+    }
+}
